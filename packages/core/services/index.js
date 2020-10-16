88fdbf74c7091b6e0170f76000a6b17b30267113
jeremy <jeremy@chaufourier.fr>
2020-10-16 23:30:11 +0200

    WIP router

--- packages/core/services/index.js
+++ packages/core/services/index.js
@@ -1,2 +1,3 @@
 module.exports.ModulesService = require('./modulesService')
 module.exports.MiddlewaresService = require('./middlewaresService')
+module.exports.RoutesService = require('./routesService')
