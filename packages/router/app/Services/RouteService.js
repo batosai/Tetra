7de37ee7fb57dac7ab288235dbc22645e75297eb
jeremy <jeremy@chaufourier.fr>
2020-12-15 00:22:55 +0100

    Add Service in route packages

---
+++ packages/router/app/Services/RouteService.js
@@ -0,0 +1,28 @@
+
+const { parseParameters, generatePrefix } = require('../../lib/utils')
+const { capitalize } = require('@tetrajs/core').utils
+
+class RouteService {
+  static async handle(namespaces, { req, res }) {
+    for (let name in namespaces) {
+      namespaces[name].forEach((route) => {
+        const prefix = generatePrefix(
+          name,
+          `${route.action}${capitalize(route.name)}`,
+        )
+
+        const r = name !== 'root' ? `/${name}${route.route}` : route.route
+
+        req[`${prefix}Path`] = res.locals[`${prefix}Path`] = (opts) =>
+          parseParameters(r, opts)
+
+        req[`${prefix}Url`] = res.locals[`${prefix}Url`] = (opts) =>
+          `${req.protocol}://${req.headers.host}${parseParameters(r, opts)}`
+
+        req[`${prefix}View`] = route.defaultView
+      })
+    }
+  }
+}
+
+module.exports = RouteService
