ffcc834be29cb4c47b464744639582c47d6604fe
jeremy <jeremy@chaufourier.fr>
2020-12-16 23:48:26 +0100

    Add function in core module

--- packages/core/lib/utils.js
+++ packages/core/lib/utils.js
@@ -4,10 +4,48 @@ const glob = require('glob')
 const { promisify } = require('util')
 
 const { ModulesService } = require('../services')
+const types = require('./types')
 
 const globSync = promisify(glob)
 
 module.exports.capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1)
+
+module.exports.permitParameters = (permit, query, blank = true) => {
+  let params = {}
+
+  for (const key of permit) {
+    if (query[key] !== undefined) {
+      if (blank) {
+        params[key] = query[key]
+      } else {
+        if (query[key] !== '') {
+          params[key] = query[key]
+        }
+      }
+    }
+  }
+
+  return params
+}
+
+module.exports.qs = (...args) => {
+  const ret = []
+
+  for (let arg of args) {
+    for (let data in arg) {
+      ret.push(encodeURIComponent(data) + '=' + encodeURIComponent(arg[data]))
+    }
+  }
+
+  return ret.join('&')
+}
+
+module.exports.mimeTypes = function(mime) {
+  if (types[mime] !== undefined) {
+    return types[mime].group
+  }
+  return 'document'
+}
 
 module.exports.exist = async function (name) {
   return new Promise((resolve) => {
